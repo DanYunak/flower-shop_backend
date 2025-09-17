556c7a9ff7d239daad7168901b18f2390189eb24
DanYunak <danyloyunak2006@gmail.com>
2025-09-18 01:43:54 +0300

    Fixed ExpressRequestInterface

--- src/types/expressRequest.interface.ts
+++ src/types/expressRequest.interface.ts
@@ -2,5 +2,9 @@ import { Request } from "express";
 import { UserEntity } from '../user/user.entity';
 
 export interface ExpressRequestInterface extends Request {
-    user?: UserEntity
-}
+    user?: UserEntity;
+    headers: {
+      authorization?: string;
+      [key: string]: string | undefined;
+    };
+  }
